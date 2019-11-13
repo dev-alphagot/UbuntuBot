04b28eae305120e224144901d3d1b8298ae31e8d
shs3182ym <56759376+shs3182ym@users.noreply.github.com>
2019-11-14 07:37:07 +0900

    소스코드 파일 2개 추가
    메인 코드와 유사깃허브 코드 업로드 왼료

---
+++ index.js
@@ -0,0 +1,15 @@
+function response(room, msg, sender, isGroupChat, replier, imageDB) {
+    var day=new Date;
+    var fullmonth=day.getMonth()+1;
+    var fullday=day.getFullYear()+"-"+fullmonth+"-"+day.getDate()+"/"+day.getHours()+"시 "+day.getMinutes()+"분 "+day.getSeconds()+"초";
+
+    if(msg.startsWith("우분투 코드제출 ")){
+        FileStream.write("storage/emulated/0/katalkbot/GitHub/"+sender+"///"+fullday,msg.replace("우분투 코드제출 ",""));
+        replier.reply("코드 제출 완료. 승인될 시 별도 전달");
+    }
+
+    if(msg=="우분투 리스폰스기본"){
+        replier.reply("function response(room, msg, sender, isGroupChat, replier, imageDB) {} 저 중괄호 사이에 코드 작성");
+    }
+
+}
